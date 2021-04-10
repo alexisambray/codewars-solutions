6d2acd8fd54e312873d76b7819f6012e75eabd75
Alexis Ambray <alexisambray@gmail.com>
2021-04-10 21:07:35 +0800

    Final solution to lowest positive problem

--- 7kyu/lowest-positive.c
+++ 7kyu/lowest-positive.c
@@ -3,63 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long sum_two_smallest_numbers(size_t n, const int numbers[n]);
+int compar(const int* p1, const int* p2) {
+  return *p1 - *p2;
+}
 
-long sum_two_smallest_numbers(size_t n, const int numbers[n]) {
-  int current_low1;  // if first 2 are the lowest
-  int current_low2;  // lowest number
-
-  int comparison1;
-  int comparison2;
-  // every new value in array to be compared with
-  // then define - if new values are lesser
-  // and if so - find out if its new value is the lowest
-  if (numbers[0] < numbers[1]) {
-    current_low1 = numbers[0];
-    current_low2 = numbers[1];
-  } else {
-    current_low1 = numbers[1];
-    current_low2 = numbers[0];
-  }
-  int ind;  // indeterminate (not precisely determined)
-  for (ind = 2; (unsigned long)ind < n; ind++) {
-    if ((numbers[ind] < current_low1) && (numbers[ind] < current_low2)) {
-      comparison1 = current_low1 - numbers[ind];
-      comparison2 = current_low2 - numbers[ind];
-      if (comparison1 > comparison2) {
-        current_low1 = numbers[ind];
-      } else {
-        current_low2 = numbers[ind];
-      }
-    } else if ((numbers[ind] >= current_low1) &&
-               (numbers[ind] < current_low2)) {
-      current_low2 = numbers[ind];
-    } else if ((numbers[ind] < current_low1) &&
-               (numbers[ind] >= current_low2)) {
-      current_low1 = numbers[ind];
-    }
-  }
-
-  return (unsigned long)(current_low1 + current_low2);
+int sum_two_smallest_numbers(size_t n, int* numbers) {
+  qsort(numbers, n, sizeof(int), compar);
+  return numbers[0] + numbers[1];
 }
 
 int main() {
-  int i, n;
-  int displayNum;
+  int size;
   int* numbers;
 
   printf("How many numbers would you like to enter? ");
-  scanf("%d", &displayNum);
+  scanf("%d", &size);
 
-  numbers = (int*)malloc(displayNum * sizeof(int));
+  numbers = (int*)malloc(size * sizeof(int));
 
-  for (i = 0; i < displayNum; i++) {
+  for (int i = 0; i < size; i++) {
     printf("Enter number #%d: ", (i + 1));
-    scanf("%d", numbers + 1);
+    scanf("%d", &numbers[i]);
   }
 
-  printf("The sum of the two lowest numbers is %" PRIu64,
-         sum_two_smallest_numbers(n, numbers));
+  printf("The sum of the two lowest numbers is %d",
+         sum_two_smallest_numbers(size, numbers));
 
   return 0;
 }
