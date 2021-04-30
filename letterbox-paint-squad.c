8cd149c607f6dcad1ffaee74647a10d547d62c17
Alexis Ambray <alexisambray@gmail.com>
2021-04-30 20:27:42 +0800

    Implemented an array of frequency digits

--- letterbox-paint-squad.c
+++ letterbox-paint-squad.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#DEFINE DIGITS 10
+
 void getRange(int* startNum, int* endNum) {
   printf("Enter the starting number: ");
   scanf(" %d", startNum);
@@ -13,6 +15,8 @@ void getRange(int* startNum, int* endNum) {
 int main() {
   int startNum, endNum;
   getRange(&startNum, &endNum);
+
+  int frequency[DIGITS];
 
   return 0;
 }
