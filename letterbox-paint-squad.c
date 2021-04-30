bd218d9625562961012418ca75475da3553be9f1
Alexis Ambray <alexisambray@gmail.com>
2021-04-30 20:20:52 +0800

    Implemented getRange function

---
+++ letterbox-paint-squad.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void getRange(int* startNum, int* endNum) {
+  printf("Enter the starting number: ");
+  scanf(" %d", startNum);
+
+  do {
+    printf("Enter the ending number: ");
+    scanf(" %d", endNum);
+  } while (*endNum < *startNum);
+}
+
+int main() {
+  int startNum, endNum;
+  getRange(&startNum, &endNum);
+
+  return 0;
+}
