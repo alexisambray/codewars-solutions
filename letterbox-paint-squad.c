4786c22ab86f52dbbb725af82a9a2e8e8765c80a
Alexis Ambray <alexisambray@gmail.com>
2021-04-30 22:18:10 +0800

    Implemented displayFrequencyTable function

--- letterbox-paint-squad.c
+++ letterbox-paint-squad.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-#DEFINE DIGITS 10
+#define DIGITS 10
 
 void getRange(int* startNum, int* endNum) {
   printf("Enter the starting number: ");
@@ -14,54 +14,40 @@ void getRange(int* startNum, int* endNum) {
   } while (*endNum < *startNum);
 }
 
-int* buildFrequencyTable(const int startNum, const endNum) {
+int charToInt(char digit) {
+  return digit - '0';
+}
+
+void getFrequencies(const char* snum, int* frequencyTable) {
+  for (int i = 0; i < strlen(snum); i++) {
+    frequencyTable[charToInt(snum[i])]++;
+  }
+}
+
+int* buildFrequencyTable(const int startNum, const int endNum) {
   int* frequencyTable = (int*)malloc(DIGITS * sizeof(int));
 
   for (int i = startNum; i <= endNum; i++) {
-    char snum[10];  
-    sprintf(snum, %d, num);
+    char snum[10];
+    sprintf(snum, "%d", i);
     getFrequencies(snum, frequencyTable);
   }
 
   return frequencyTable;
 }
 
-void getFrequencies(const char* snum, int* frequencyTable) {
-  for (int i = 0; i < strlen(snum); i++) {
-    frequencyTable[charToInt(snum[0])]++;
+void displayFrequencyTable(int* frequencyTable) {
+  for (int i = 0; i < DIGITS; i++) {
+    printf("%d is painted %d time/s\n", i, frequencyTable[i]);
   }
-}
-
-int charToInt(char digit) {
-  return digit - '0';
 }
 
 int main() {
   int startNum, endNum;
   getRange(&startNum, &endNum);
 
-  int* frequencyTable;
-  buildFrequencyTable(startNum, endNum);
+  int* frequencyTable = buildFrequencyTable(startNum, endNum);
+  displayFrequencyTable(frequencyTable);
 
   return 0;
 }
-
-/* 
-310 START
-311
-312
-313
-314
-315 END
-
-0 --> 0
-1 --> 0
-2 --> 0
-3 --> 0
-4 --> 0
-5 --> 0
-6 --> 0
-7 --> 0
-8 --> 0
-9 --> 0
-*/
