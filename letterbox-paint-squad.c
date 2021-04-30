1739f310ff0a71ef1cb14024b5b108a27053383f
Alexis Ambray <alexisambray@gmail.com>
2021-04-30 21:39:00 +0800

    Implemented frequency table function

--- letterbox-paint-squad.c
+++ letterbox-paint-squad.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #DEFINE DIGITS 10
 
@@ -12,11 +14,54 @@ void getRange(int* startNum, int* endNum) {
   } while (*endNum < *startNum);
 }
 
+int* buildFrequencyTable(const int startNum, const endNum) {
+  int* frequencyTable = (int*)malloc(DIGITS * sizeof(int));
+
+  for (int i = startNum; i <= endNum; i++) {
+    char snum[10];  
+    sprintf(snum, %d, num);
+    getFrequencies(snum, frequencyTable);
+  }
+
+  return frequencyTable;
+}
+
+void getFrequencies(const char* snum, int* frequencyTable) {
+  for (int i = 0; i < strlen(snum); i++) {
+    frequencyTable[charToInt(snum[0])]++;
+  }
+}
+
+int charToInt(char digit) {
+  return digit - '0';
+}
+
 int main() {
   int startNum, endNum;
   getRange(&startNum, &endNum);
 
-  int frequency[DIGITS];
+  int* frequencyTable;
+  buildFrequencyTable(startNum, endNum);
 
   return 0;
 }
+
+/* 
+310 START
+311
+312
+313
+314
+315 END
+
+0 --> 0
+1 --> 0
+2 --> 0
+3 --> 0
+4 --> 0
+5 --> 0
+6 --> 0
+7 --> 0
+8 --> 0
+9 --> 0
+*/
